a7b4f710d5cc8c28cfe08a51971eb3c5927d15e1
Thanh Tùng <electricthanhtung@gmail.com>
2026-08-04 23:26:09 +0700

    Update flintos.internal.AMS.java

--- flintos/java/src/midp/flintos/internal/AMS.java
+++ flintos/java/src/midp/flintos/internal/AMS.java
@@ -5,7 +5,6 @@ import javax.microedition.midlet.MIDletStateChangeException;
 
 class AMS {
     public static void startApp(Class<? extends MIDlet> mainClass) throws Exception {
-        System.out.println("Start" + mainClass.getName());
         MIDlet midlet = mainClass.getConstructor().newInstance();
         mainClass.getMethod("startApp").invoke(midlet);
     }
