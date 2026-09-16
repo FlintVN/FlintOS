d99af8e5f3deaa5d9588096ad8922b6bc1ac303a
Thanh Tùng <electricthanhtung@gmail.com>
2026-09-16 11:35:15 +0700

    Add TextView

---
+++ flintos/java/src/flintos/flint/ui/TextView.java
@@ -0,0 +1,146 @@
+package flint.ui;
+
+import flint.drawing.Size;
+import flint.drawing.Font;
+import flint.drawing.Color;
+import flint.drawing.Image;
+import flint.drawing.ClipMode;
+import flint.drawing.Graphics;
+
+public class TextView extends View {
+    protected String text;
+    protected Font font;
+    protected Color textColor;
+
+    protected int topLeftRadius;
+    protected int topRightRadius;
+    protected int bottomLeftRadius;
+    protected int bottomRightRadius;
+
+    protected int paddingLeft;
+    protected int paddingTop;
+    protected int paddingRight;
+    protected int paddingBottom;
+
+    public TextView() {
+        width = View.WRAP_CONTENT;
+        height = View.WRAP_CONTENT;
+        font = Theme.defaultTheme.defaultFont();
+        textColor = Theme.defaultTheme.textColor();
+    }
+
+    @Override
+    protected void onDraw(Graphics g) {
+        if(background != null) {
+            if(background instanceof Color color) {
+                if(color.getAlpha() > 0)
+                    g.fillRoundRect(color, x, y, actualWidth, actualHeight, topLeftRadius, topRightRadius, bottomRightRadius, bottomLeftRadius);
+            }
+            else
+                g.drawImage((Image)background, x, y, actualWidth, actualHeight);
+        }
+
+        if(text == null || textColor == null) return;
+
+        int gClipX = g.getClipX();
+        int gClipY = g.getClipY();
+        int gClipW = g.getClipWidth();
+        int gClipH = g.getClipHeight();
+
+        int x1 = paddingLeft;
+        int y1 = paddingTop;
+        int x2 = actualWidth - paddingRight;
+        int y2 = actualHeight - paddingBottom;
+
+        g.setClip(this.x + x1, this.y + y1, x2 - x1, y2 - y1, ClipMode.INTERSECT);
+        g.drawString(text, font, textColor, x + paddingLeft, y + paddingTop);
+
+        g.setClip(gClipX, gClipY, gClipW, gClipH, ClipMode.REPLACE);
+    }
+    
+    @Override
+    protected void updateActualWidth(int availableW) {
+        if(width == View.WRAP_CONTENT || (width == View.MATCH_PARENT && availableW < 0)) {
+            int contentW = Graphics.measureStringWidth(text, font) + paddingLeft + paddingRight;
+            actualWidth = width >= 0 ? width : ((width == View.WRAP_CONTENT || availableW < 0) ? contentW : availableW);
+        }
+        else
+            actualWidth = width >= 0 ? width : availableW;
+    }
+
+    @Override
+    protected void updateActualHeight(int availableH) {
+        if(height == View.WRAP_CONTENT || (height == View.MATCH_PARENT && availableH < 0)) {
+            int contentH = Graphics.measureStringHeight(text, font) + paddingTop + paddingBottom;
+            actualHeight = height >= 0 ? height : ((height == View.WRAP_CONTENT || availableH < 0) ? contentH : availableH);
+        }
+        else
+            actualHeight = height >= 0 ? height : availableH;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public void setFont(Font font) {
+        if(font == null)
+            throw new NullPointerException("font cannot be null");
+        this.font = font;
+    }
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(Color color) {
+        textColor = color;
+    }
+
+    public CornerRadius getCornerRadius() {
+        return new CornerRadius(topLeftRadius, topRightRadius, bottomRightRadius, bottomLeftRadius);
+    }
+
+    public void setCornerRadius(int radius) {
+        setCornerRadius(radius, radius, radius, radius);
+    }
+
+    public void setCornerRadius(CornerRadius radius) {
+        setCornerRadius(radius.topLeft, radius.topRight, radius.bottomRight, radius.bottomLeft);
+    }
+
+    public void setCornerRadius(int topLeft, int topRight, int bottomRight, int bottomLeft) {
+        this.topLeftRadius = topLeft;
+        this.topRightRadius = topRight;
+        this.bottomLeftRadius = bottomLeft;
+        this.bottomRightRadius = bottomRight;
+    }
+
+    public Padding getPading() {
+        return new Padding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+    }
+
+    public void setPadding(int padding) {
+        setPadding(padding, padding, padding, padding);
+    }
+
+    public void setPadding(Padding padding) {
+        setPadding(padding.left, padding.top, padding.right, padding.bottom);
+    }
+
+    public void setPadding(int left, int top, int right, int bottom) {
+        if(left < 0 || top < 0 || right < 0 || bottom < 0)
+            throw new IllegalArgumentException("TextView does not support padding with negative numbers");
+        paddingLeft = left;
+        paddingTop = top;
+        paddingRight = right;
+        paddingBottom = bottom;
+    }
+}
