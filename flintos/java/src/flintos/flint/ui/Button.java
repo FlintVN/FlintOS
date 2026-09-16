8b47bfe979a89b4cb8fafe7d7c9723338c23f696
Thanh Tùng <electricthanhtung@gmail.com>
2026-09-16 11:04:20 +0700

    Add Button

---
+++ flintos/java/src/flintos/flint/ui/Button.java
@@ -0,0 +1,151 @@
+package flint.ui;
+
+import flint.drawing.Size;
+import flint.drawing.Font;
+import flint.drawing.Color;
+import flint.drawing.ClipMode;
+import flint.drawing.Graphics;
+
+public class Button extends PanelView {
+    protected String text;
+    protected Font font;
+    protected Color textColor;
+
+    protected int paddingLeft;
+    protected int paddingTop;
+    protected int paddingRight;
+    protected int paddingBottom;
+
+    public Button() {
+        background = Theme.defaultTheme.primaryColor();
+        borderColor = Theme.defaultTheme.borderColor();
+
+        font = Theme.defaultTheme.defaultFont();
+        textColor = Theme.defaultTheme.textColor();
+
+        width = 65;
+        height = 30;
+
+        int defaultRadius = Theme.defaultTheme.cornerRadius();
+        topLeftRadius = defaultRadius;
+        topRightRadius = defaultRadius;
+        bottomLeftRadius = defaultRadius;
+        bottomRightRadius = defaultRadius;
+
+        paddingLeft = 2;
+        paddingTop = 2;
+        paddingRight = 2;
+        paddingBottom = 2;
+    }
+
+    @Override
+    protected void onDraw(Graphics g) {
+        super.onDraw(g);
+
+        if(text == null) return;
+
+        int gClipX = g.getClipX();
+        int gClipY = g.getClipY();
+        int gClipW = g.getClipWidth();
+        int gClipH = g.getClipHeight();
+
+        int thk = getBorderThickness();
+
+        int x1 = thk;
+        int y1 = thk;
+        int x2 = actualWidth - thk;
+        int y2 = actualHeight - thk;
+
+        if(paddingLeft > 0) x1 += paddingLeft;
+        if(paddingTop > 0) y1 += paddingTop;
+        if(paddingRight > 0) x2 -= paddingRight;
+        if(paddingBottom > 0) y2 -= paddingBottom;
+
+        g.setClip(this.x + x1, this.y + y1, x2 - x1, y2 - y1, ClipMode.INTERSECT);
+
+        int txtW = Graphics.measureStringWidth(text, font);
+        int txtH = Graphics.measureStringHeight(null, font);
+        int x = (actualWidth - txtW) / 2 + paddingLeft - paddingRight + this.x;
+        int y = (actualHeight - txtH) / 2 + paddingTop - paddingBottom + this.y;
+        g.drawString(text, font, textColor, x, y);
+
+        g.setClip(gClipX, gClipY, gClipW, gClipH, ClipMode.REPLACE);
+    }
+
+    @Override
+    protected void updateActualWidth(int availableW) {
+        if(width == View.WRAP_CONTENT || (width == View.MATCH_PARENT && availableW < 0)) {
+            int contentW = getBorderThickness() << 1;
+
+            if(paddingLeft > 0) contentW += paddingLeft;
+            if(paddingRight > 0) contentW += paddingRight;
+
+            contentW += Graphics.measureStringWidth(text, font);
+
+            actualWidth = width >= 0 ? width : ((width == View.WRAP_CONTENT || availableW < 0) ? contentW : availableW);
+        }
+        else
+            actualWidth = width >= 0 ? width : availableW;
+    }
+
+    @Override
+    protected void updateActualHeight(int availableH) {
+        if(height == View.WRAP_CONTENT || (height == View.MATCH_PARENT && availableH < 0)) {
+            int contentH = getBorderThickness() << 1;
+
+            if(paddingTop > 0) contentH += paddingTop;
+            if(paddingBottom > 0) contentH += paddingBottom;
+
+            contentH += Graphics.measureStringHeight(text, font);
+
+            actualHeight = height >= 0 ? height : ((height == View.WRAP_CONTENT || availableH < 0) ? contentH : availableH);
+        }
+        else
+            actualHeight = height >= 0 ? height : availableH;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public void setFont(Font font) {
+        if(font == null)
+            throw new NullPointerException("font cannot be null");
+        this.font = font;
+    }
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(Color color) {
+        textColor = color;
+    }
+
+    public Padding getPading() {
+        return new Padding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+    }
+
+    public void setPadding(int padding) {
+        setPadding(padding, padding, padding, padding);
+    }
+
+    public void setPadding(Padding padding) {
+        setPadding(padding.left, padding.top, padding.right, padding.bottom);
+    }
+
+    public void setPadding(int left, int top, int right, int bottom) {
+        paddingLeft = left;
+        paddingTop = top;
+        paddingRight = right;
+        paddingBottom = bottom;
+    }
+}
---
+++ flintos/java/src/flintos/flint/ui/PanelView.java
@@ -0,0 +1,82 @@
+package flint.ui;
+
+import flint.drawing.Color;
+import flint.drawing.Image;
+import flint.drawing.Graphics;
+
+class PanelView extends View {
+    protected Color borderColor;
+
+    protected int topLeftRadius;
+    protected int topRightRadius;
+    protected int bottomLeftRadius;
+    protected int bottomRightRadius;
+
+    public PanelView() {
+
+    }
+
+    @Override
+    protected void onDraw(Graphics g) {
+        int r1 = topLeftRadius;
+        int r2 = topRightRadius;
+        int r3 = bottomLeftRadius;
+        int r4 = bottomRightRadius;
+
+        if(background != null) {
+            if(background instanceof Color color) {
+                if(color.getAlpha() > 0)
+                    g.fillRoundRect(color, x, y, actualWidth, actualHeight, r1, r2, r3, r4);
+            }
+            else
+                g.drawImage((Image)background, x, y, actualWidth, actualHeight);
+        }
+
+        if(borderColor != null && borderColor.getAlpha() > 0) {
+            int w = actualWidth - 1;
+            int h = actualHeight - 1;
+            g.drawRoundRect(borderColor, x, y, w, h, r1, r2, r3, r4);
+        }
+    }
+
+    @Override
+    protected void updateActualWidth(int availableW) {
+        actualWidth = width >= 0 ? width : ((width == View.WRAP_CONTENT || availableW < 0) ? 0 : availableW);
+    }
+
+    @Override
+    protected void updateActualHeight(int availableH) {
+        actualHeight = height >= 0 ? height : ((height == View.WRAP_CONTENT || availableH < 0) ? 0 : availableH);
+    }
+
+    int getBorderThickness() {
+        return (borderColor != null && borderColor.getAlpha() > 0) ? 1 : 0;
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(Color color) {
+        borderColor = color;
+    }
+
+    public CornerRadius getCornerRadius() {
+        return new CornerRadius(topLeftRadius, topRightRadius, bottomRightRadius, bottomLeftRadius);
+    }
+
+    public void setCornerRadius(int radius) {
+        setCornerRadius(radius, radius, radius, radius);
+    }
+
+    public void setCornerRadius(CornerRadius radius) {
+        setCornerRadius(radius.topLeft, radius.topRight, radius.bottomRight, radius.bottomLeft);
+    }
+
+    public void setCornerRadius(int topLeft, int topRight, int bottomRight, int bottomLeft) {
+        this.topLeftRadius = topLeft;
+        this.topRightRadius = topRight;
+        this.bottomLeftRadius = bottomLeft;
+        this.bottomRightRadius = bottomRight;
+    }
+}
